32ef764044c01bf771be9599bfefa50a1bfcac9e
Summer <swatson9008@yahoo.com>
2023-09-27 15:37:39 -0400

    placed the main layout up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+import { ReactDOM } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout";
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
