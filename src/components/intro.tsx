a8b11d5b88fb1e76ecf2ec342137c3a9e91d27d0
Summer <swatson9008@yahoo.com>
2023-09-30 12:32:40 -0400

    animated the intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,11 +1,35 @@
-import { IntroContainer } from "../styled/introStyled"
+import { IntroContainer } from "../styled/introStyled";
+import { Animate, AnimateGroup } from "react-simple-animate";
 
-export default function Intro(){
-
-    return(
-        <IntroContainer>
-            <span><h1>Summer Watson</h1></span>
-            <span><h2>front end web developer</h2></span>
-        </IntroContainer>
-    )
+export default function Intro({ play, onCompleteCallBack }) {
+  return (
+    <IntroContainer>
+      <AnimateGroup play>
+        <div>
+          <Animate
+            play={true}
+            duration={1}
+            delay={0.3}
+            start={{ opacity: "0", transform: "translate(0, 40px)" }}
+            end={{ opacity: "1", transform: "translate(0, 0)" }}
+            sequenceIndex={0}
+            onComplete={onCompleteCallBack}
+          >
+            <h1>Summer Watson</h1>
+          </Animate>
+          <Animate
+          play={true}
+          duration={1}
+          delay={0.3}
+          start={{ opacity: "0", transform: "translate(0, 40px)" }}
+          end={{ opacity: "1", transform: "translate(0, 0)" }}
+          sequenceIndex={1}
+          onComplete={onCompleteCallBack}
+          >
+            <h2>front end web developer</h2>
+          </Animate>
+        </div>
+      </AnimateGroup>
+    </IntroContainer>
+  );
 }
