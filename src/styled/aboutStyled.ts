e3e58a397b7f034ed83e0bc8f88f6bf8e67ae6bb
Summer <swatson9008@yahoo.com>
2023-10-03 12:02:13 -0400

    minor css fix for about section

--- src/styled/aboutStyled.ts
+++ src/styled/aboutStyled.ts
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 40px;
+  justify-content: center;
+  padding: 30px;
+
+  @media screen and (max-width: 899px) {
+    margin: 10px;
     justify-content: center;
-    padding: 50px;
-
-    
-
+    padding: 10px;
+  }
 `;
