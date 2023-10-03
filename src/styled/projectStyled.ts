7f2feb31108f32d1c27aee7c70802a70e72c3fe2
Summer <swatson9008@yahoo.com>
2023-10-03 11:19:23 -0400

    replacing project placeholder

---
+++ src/styled/projectStyled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ProjectContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 40px;
+    justify-content: center;
+    padding: 50px;
+
+    
+
+`;
